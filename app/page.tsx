e8b781da547aca1eff3d3e453d501f6b967702c6
linoles <cernikovdenis081@gmail.com>
2025-07-23 16:30:53 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,26 @@
 "use client";
 
 import { useEffect } from "react";
+import { Telegraf } from "telegraf";
+
+const bot = new Telegraf("7665933078:AAEk1IIIAafXQGki6i9tejLv4BBQ8MqWLuc");
+
+bot.on("message", (ctx) => {
+  ctx.sendInvoice({
+    title: "Test",
+    description: "Test",
+    start_parameter: "test",
+    currency: "XTR",
+    prices: [{ label: "Test", amount: 1 }],
+    payload: JSON.stringify({ data: "Test" }),
+    provider_token: "test",
+  })
+});
+
+bot.launch();
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
 declare global {
   interface Window {
