142fc137cbad619b7cfe98b152fd4ed82c39621a
linoles <cernikovdenis081@gmail.com>
2025-07-23 17:24:54 +0300

    update

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,13 @@ const nextConfig: NextConfig = {
   /* config options here */
   webpack: (config: any, options: any) => {
     console.log(options.webpack.version); // Should be webpack v5 now
-    config.experiments = {};
+    config.experiments = {
+      layers: true, // Enable experiments.layers to fix the error
+    };
     config.target = 'node'; // Add the target: 'node' here
     return config;
   },
 };
 
 export default nextConfig;
+
