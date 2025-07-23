869464132d7bdf2250a03a4a300f2655621cdab3
linoles <cernikovdenis081@gmail.com>
2025-07-23 17:19:07 +0300

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config: any, options: any) => {
+    console.log(options.webpack.version); // Should be webpack v5 now
+    config.experiments = {};
+    config.target = 'node'; // Add the target: 'node' here
+    return config;
+  },
 };
 
 export default nextConfig;
