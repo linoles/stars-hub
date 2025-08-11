00d84b9810b99a1a733ff5bb121c8dad60360fbb
linoles <cernikovdenis081@gmail.com>
2025-08-11 22:35:41 +0300

    update

--- app/api/bot/route.ts
+++ app/api/bot/route.ts
@@ -98,7 +98,7 @@ bot.action(/^show(?:Winners|RequiredTimes|RequiredRow)$/, async (ctx) => {
     });
     return;
   }
-  ctx.answerCbQuery(`⚙ Текущая настройка: ${row.ludka[ctx.match[0].slice(4)]}`, {
+  ctx.answerCbQuery(`⚙ Текущая настройка: ${JSON.stringify(ctx.match)}`, {
     show_alert: true,
     cache_time: 0,
   });
