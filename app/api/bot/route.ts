77365130484a83b67234d0e7f1cba4f6724c3d2e
linoles <cernikovdenis081@gmail.com>
2025-08-14 21:42:49 +0300

    update

--- app/api/bot/route.ts
+++ app/api/bot/route.ts
@@ -610,7 +610,7 @@ const finishGame = async (ctx: any, from: number) => {
 
     await updateLeaderboard(ctx, from);
 
-    bot.telegram.sendMessage(7441988500, `${JSON.stringify(Object.entries(globalGameState.row.game.doneUsers).filter(([_, data]: any) => data?.progress >= globalGameState?.row.game.moves))} + ${JSON.stringify(globalGameState.row.game.doneUsers)}`);
+    bot.telegram.sendMessage(7441988500, `${JSON.stringify(Object.entries(globalGameState.row.game.doneUsers).filter(([_, data]: any) => data?.progress >= globalGameState?.row.game.moves).map(([user, data]: any) => ({user, progress: data.points})))} + ${JSON.stringify(Object.fromEntries(Object.entries(globalGameState.row.game.doneUsers).map(([user, data]: any) => [user, {progress: data.points, points: data.points}])))}`);
     if (Object.entries(globalGameState.row.game.doneUsers).filter(([_, data]: any) => data?.progress >= globalGameState?.row.game.moves).length >= globalGameState.row.game.space) {
       await endGlobalGame(ctx);
     }
