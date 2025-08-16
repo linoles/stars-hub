e4651b9dfccec343efe734b8c59aa014f8dca1fa
linoles <cernikovdenis081@gmail.com>
2025-08-16 18:57:21 +0300

    update

--- app/api/bot/route.ts
+++ app/api/bot/route.ts
@@ -1380,9 +1380,8 @@ bot.on("message", async (ctx) => {
         return;
       } else if (msg.toLowerCase().startsWith("/profile top")) {
         const place = Number(msg.split("top")[1]);
-        ctx.reply(`${place}`);
-        const top = Object.entries(row.doneUsers)
-          .sort((a: any, b: any) => b[1].points - a[1].points)[place]
+        const top = Object.entries(row.game.doneUsers)
+          .sort((a: any, b: any) => b[1].points - a[1].points)[place - 1]
         ctx.reply(JSON.stringify(top));
         return;
       }
