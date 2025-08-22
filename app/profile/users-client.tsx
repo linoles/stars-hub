a608b669efdc3848a2f98977f6d60e56ad142911
linoles <cernikovdenis081@gmail.com>
2025-08-22 18:44:30 +0300

    update

--- app/profile/users-client.tsx
+++ app/profile/users-client.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 import { inter } from "../fonts";
 import BottomMenu from "../lib/bottomMenu";
 import { User } from "../users-client";
-import UserAvatar from "../lib/userAvatar";
 
 declare global {
   interface Window {
@@ -90,7 +89,16 @@ export default function ClientComponent({ initialUsers }: { initialUsers: User[]
             <div className={"text-3xl font-bold text-casino-gold/50 overflow-hidden text-ellipsis whitespace-nowrap max-w-[300px] " + inter.className}>{curUser.tgUsername !== null && curUser.tgUsername !== "" ? `@${curUser.tgUsername}` : `id${curUser.tgId}`}</div>
           </div>
           <div className="relative z-10 text-center space-y-6 border-2 rounded-full border-b-red-400">
-            <UserAvatar userId={7441988500} size={200} className="border-2 border-white" />
+            <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+              width="200" height="200" viewBox="0 0 512.000000 512.000000"
+              preserveAspectRatio="xMidYMid meet">
+
+              <g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
+                fill="#2B2B2B" stroke="none">
+                <path
+                  d="M2390 4253 c-269 -43 -478 -150 -671 -342 -185 -186 -286 -378 -335 -636 -22 -120 -15 -371 15 -495 56 -241 154 -447 306 -646 85 -111 95 -177 40 -261 -8 -12 -80 -55 -163 -97 -286 -145 -506 -316 -700 -541 -101 -117 -122 -154 -122 -210 0 -131 147 -213 258 -144 13 8 59 58 104 112 169 204 366 356 643 495 158 79 211 120 267 206 98 153 109 357 26 521 -12 22 -53 83 -92 135 -169 225 -259 477 -258 725 0 151 23 242 93 384 116 231 344 402 599 451 93 18 258 14 354 -9 326 -78 574 -337 642 -671 13 -65 15 -112 11 -205 -9 -174 -47 -309 -138 -486 -66 -128 -146 -236 -245 -328 -136 -127 -270 -190 -442 -209 -74 -8 -98 -14 -125 -35 -67 -51 -86 -145 -45 -219 46 -81 119 -100 280 -73 104 17 178 41 282 90 32 15 59 25 60 24 2 -2 16 -29 31 -58 15 -30 52 -81 82 -112 46 -48 79 -70 199 -130 253 -126 420 -248 589 -434 175 -192 176 -192 226 -200 128 -19 235 110 189 230 -29 77 -272 331 -436 456 -131 100 -209 149 -384 239 -135 70 -147 78 -168 120 -31 60 -25 128 17 185 203 273 311 506 356 769 32 188 14 420 -45 598 -136 407 -499 720 -918 793 -78 14 -317 19 -382 8z" />
+              </g>
+            </svg>
           </div>
           <div className="flex justify-center pt-6 pb-3">
             <div className="text-center pr-8 border-r border-casino-gold/20">
